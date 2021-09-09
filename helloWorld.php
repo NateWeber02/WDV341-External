63e48ee20bff5bc2ef0fb18b7ff2506b39ca219d
NateWeber02 <nate.weber.work@gmail.com>
2021-09-09 13:15:22 -0500

    Initial commit

---
+++ helloWorld.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+	
+<body>
+	<h1>WDV341 Into to PHP</h1>
+	<h2>PHP Basics</h2>
+	
+	<?php
+	
+		echo "<h3> This output comes from PHP </h3>";
+	
+	?>
+	
+</body>
+</html>
